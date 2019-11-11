e8a220d4b007f22d4856b9f6d31e0c5748e171c0
mojsmojsknas <55138217+mojsmojsknas@users.noreply.github.com>
2019-11-11 10:52:42 +0100

    m

---
+++ NovemberProjekt/NovemberProjekt/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberProjekt
+{
+    /*2019-11-11 
+    Jag har börjat exprimetera lite gran med properties och inkapsling då det värkar najs.
+
+    */
+    class Enemy
+    {
+        public int hp = 0;
+
+        public int Hp
+        {
+            get
+            {
+                return hp;
+            }
+            set
+            {
+                hp = value;
+            }
+
+        }
+
+
+
+
+
+    }
+}
--- NovemberProjekt/NovemberProjekt/Program.cs
+++ NovemberProjekt/NovemberProjekt/Program.cs
@@ -6,13 +6,34 @@ using System.Threading.Tasks;
 
 namespace NovemberProjekt
 {
+    //Jag tänker nog göra något form av gamestate spel
+    //Jag har valt att försöka göra någon form av gamestate projekt fast med klasser. Men majoriteten av lektionen har gått åt till att kolla igenom mina tidigare arbeten och uppgiftsbeskrivningen för insperation
+
+    /*2019-11-11 
+    Jag har börjat exprimetera lite gran med properties och inkapsling då det värkar najs.
+    Primärt i Enemy klassen
+    */
     class Program
     {
         static void Main(string[] args)
         {
-            //Jag tänker nog göra något form av gamestate spel
-            //Jag har valt att försöka göra någon form av gamestate projekt fast med klasser. Men majoriteten av lektionen har gått åt till att kolla igenom mina tidigare arbeten och uppgiftsbeskrivningen för insperation
+            
+            bool quit = false;
 
+
+            while (quit == false)
+            {
+
+
+
+
+
+                
+
+            }
+
+
+            Console.ReadKey();
 
         }
     }
