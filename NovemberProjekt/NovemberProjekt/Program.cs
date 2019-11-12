2c15399bdb0318b47fc1f009fffb2e92ed6b4a5c
mojsmojsknas <55138217+mojsmojsknas@users.noreply.github.com>
2019-11-12 12:53:48 +0100

    m

--- NovemberProjekt/NovemberProjekt/Program.cs
+++ NovemberProjekt/NovemberProjekt/Program.cs
@@ -13,6 +13,14 @@ namespace NovemberProjekt
     Jag har börjat exprimetera lite gran med properties och inkapsling då det värkar najs.
     Primärt i Enemy klassen
     */
+    /*2019-11-12
+    Jag har mest läst på mer om det jag var osäker på och försökt samla mina tankar om 
+    vad exakt det är jag ska göra för sorts projekt. Jag har kännt mig lite osäker på 
+    klasser och speciellt arv och jag tror att jag kommer att kunna ha stor nytta av 
+    just arv till projektet. Jag kommer nog vara tvungen att programera hemma :( eller
+    göra ett mindre projekt än vad jag tänkt mig. Vi får se
+    */
+
     class Program
     {
         static void Main(string[] args)
@@ -23,8 +31,9 @@ namespace NovemberProjekt
 
             while (quit == false)
             {
+                //skapar en enemy
                 Enemy e = new Enemy();
-
+                //hämtar get från Hp och skriver ut det 
                 Console.WriteLine(e.Hp);
                 Console.ReadKey();
 
